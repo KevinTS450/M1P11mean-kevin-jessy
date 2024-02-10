8a1c26a7c28657c3248430f352d76ddd3df9128f
jessVatilab <jessy@vatilab.com>
2024-02-10 18:09:40 +0300

    add controller to API

--- api/api.js
+++ api/api.js
@@ -5,9 +5,20 @@ const authenticateToken = require("../JWT/jwtMiddleware");
 const handlerLogin = require("../Controller/AuthController/loginHandler");
 const HandlerUser = require("../Controller/UserController/UserHandler");
 
+/**************************                 User            ***********************************/
+
 router.post("/inscription", handlerInscription);
 router.get("/get-user", authenticateToken, HandlerUser.GetUserByToken);
 router.post("/login", handlerLogin);
 router.get("/AllUser", HandlerUser.GetAllUser);
+
+
+/**************************                 MOBILEMONEY            ***********************************/
+
+router.post("/mobileMoney/create", mobileMoneyController.createMobileMoney)
+router.get("/mobileMoney/findAll", mobileMoneyController.GetAllMobileMoney)
+router.get("/mobileMoney/findById/:id", mobileMoneyController.GetMobileMoneyById)
+router.put("/mobileMoney/updateById/:id", mobileMoneyController.updateMobileMoney)
+router.delete("/mobileMoney/deleteById/:id", mobileMoneyController.deleteMobileMoney)
 
 module.exports = router;
