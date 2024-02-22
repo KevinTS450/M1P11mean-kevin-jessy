807f6adb79b87751caf0da3d6c9096b37c1297dc
Jess <jessyowen48@gmail.com>
2024-02-22 15:07:45 +0300

    Fix createRDV

--- Client/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ Client/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,8 +1,7 @@
 import { Routes } from "@angular/router";
 
-import { IconsComponent } from "../../pages/icons/icons.component";
-import { MapsComponent } from "../../pages/maps/maps.component";
-import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
+// import { IconsComponent } from "../../pages/icons/icons.component";
+// import { MapsComponent } from "../../pages/maps/maps.component";
 
 import { UserProfileComponent } from "../../pages/user-profile/user-profile.component";
 import { TablesComponent } from "../../pages/tables/tables.component";
@@ -22,8 +21,8 @@ export const AdminLayoutRoutes: Routes = [
     { path: "dashboard", component: DashboardComponent },
     { path: "user-profile", component: UserProfileComponent },
     { path: "tables", component: TablesComponent },
-    { path: "icons", component: IconsComponent },
-    { path: "maps", component: MapsComponent },
+    // { path: "icons", component: IconsComponent },
+    // { path: "maps", component: MapsComponent },
     { path: "serviceType", component: ServiceTypeComponent },
     { path: "AjoutService", component: AjouterServiceComponent },
     { path: "ModifierService/:id", component: ModifierServiceComponent },
