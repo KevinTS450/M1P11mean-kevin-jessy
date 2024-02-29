b16ab412fae4cf4fbe8561f32cb4614b6720c36d
kevinTS450 <kevin.maharo@katappult.com>
2024-02-29 21:55:48 +0300

    fix uploads[kevin]

--- service/Upload/upload.js
+++ service/Upload/upload.js
@@ -3,12 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const destinationPath = path.join(
-      __dirname,
-      "..",
-      "..",
-      "/Client/src/assets/uploads"
-    );
+    const destinationPath = path.join(__dirname, "..", "..", "");
     cb(null, destinationPath);
   },
   filename: function (req, file, cb) {
